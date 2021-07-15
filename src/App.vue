3e5f662aa0c9df195c68899cc56ec4e516573e32
Simon Burg <semburg.sb@gmail.com>
2021-07-15 11:05:37 +0200

    starting. first directives, structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div>
+        
+        <button v-on:click="addLike" >Like</button>
+        <button v-on:click="addDislike">Dislike</button>
+
+        <div>Number of likes: <strong>{{ likes }} </strong> </div>
+        <div>Number of dislikes: <strong>{{ dislikes }} </strong> </div>
+        
+    </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+        }
+    },
 
-#nav {
-  padding: 30px;
-}
+    methods: {
+        addLike() {
+            this.likes += 1;
+        },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+        addDislike(){
+            this.dislikes += 1;
+        }
+    },  
 }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="">
+    
 </style>
